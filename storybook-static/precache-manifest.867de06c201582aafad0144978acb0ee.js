a06ce5faf41d0325d94b8a2bb5e64bbd7b05e032
Aihaiti Abudureheman <aihaiti.abudureheman@cybercom.com>
2020-10-28 13:41:27 +0200

    Added build scripts

---
+++ storybook-static/precache-manifest.867de06c201582aafad0144978acb0ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dfc83b498d97e0d0b3bbc6cb4bdd5c8",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.779b21b1b7401a471f27.bundle.js"
+  },
+  {
+    "url": "runtime~main.779b21b1b7401a471f27.bundle.js"
+  },
+  {
+    "url": "vendors~main.779b21b1b7401a471f27.bundle.js"
+  },
+  {
+    "url": "vendors~main.779b21b1b7401a471f27.bundle.js.LICENSE.txt"
+  }
+]);
